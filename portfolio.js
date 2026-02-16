244ce79f40cb32081a0de290c616b74c660bd4ed
faseehansari <faseehahmed009@gmail.com>
2026-02-16 23:45:02 +0500

    second

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,3 @@
-// console.log(Math.random() *10 + 1);
-// let otp = Math.floor(Math.random() *10)
-
-// console.log(otp);
-
-// let onetp = Math.floor(1000 + Math.random() * 9000);
-
-// console.log(onetp);
-
 const openBtn = document.querySelector(".openbtn");
 const navLinks = document.querySelector(".nav-links");
 
